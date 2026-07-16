ce3d8629b42ae22c205637061d9229ac9ddf1348
jkwanhow <58902790+jkwanhow@users.noreply.github.com>
2026-07-17 01:06:32 +1000

    actually nearly a CLI app
    Btw, major cleanup needed once things are working.

--- cli-wordle/main.go
+++ cli-wordle/main.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"os"
 	"strings"
+	"unicode"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 const LIVES = 6
@@ -23,12 +26,6 @@ func GetNotCorrectSquare(c rune, a string) string {
 	}
 
 	return WRONG
-}
-
-func ReplaceAtIndex(s string, r rune, i int) string {
-	output := []rune(s)
-	output[i] = r
-	return string(output)
 }
 
 func CreateSquareOutput(a string, g string) string {
@@ -96,6 +93,7 @@ func CreateDictionary() map[string]bool {
 	return dict
 }
 
+/*
 func main() {
 	const answer = "pshaw"
 	length := GetLength(answer)
@@ -126,4 +124,134 @@ func main() {
 		fmt.Printf("Nice")
 	}
 
+}
+*/
+
+type model struct {
+	guesses        [6][5]string
+	letterStates   [6][5]int
+	curRow, curCol int
+	answer         string
+	dictionary     map[string]bool
+	invalidState   string
+}
+
+const (
+	Reset = "\033[0m"
+	// The numbers define the color:
+	// 42 = green background, 37 = white text
+	Green  = "\033[42;37m"
+	Yellow = "\033[43;37m"
+
+	// 100 = dark gray background
+	Gray = "\033[100;37m"
+)
+
+// View implements tea.Model.
+func (m model) View() string {
+	// The header
+	s := "WORDLE?!\n\n"
+	// Iterate over our choices
+	for row, word := range m.guesses {
+		rowState := m.letterStates[row]
+		for col, letter := range word {
+			colorState := Gray
+			if rowState[col] == 1 {
+				colorState = Green
+			} else if rowState[col] == -1 {
+				colorState = Yellow
+			}
+
+			s += fmt.Sprintf("%s[ %s ]%s", colorState, letter, Reset)
+		}
+		s += fmt.Sprint("\n")
+	}
+
+	if m.invalidState == "length" {
+		s += "\n*Not enough letters*\n"
+	} else if m.invalidState == "non-word" {
+		s += "\n*Not in word list*\n"
+	}
+
+	// The footer
+	s += "\nPress ctrl+c to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
+
+func initialModel() model {
+	dictionary := CreateDictionary()
+	return model{
+		curRow:     0,
+		curCol:     0,
+		dictionary: dictionary,
+		answer:     "boats",
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	//key is pressed
+	case tea.KeyMsg:
+
+		// what key is pressed
+		switch msg.String() {
+
+		// close or exit the program
+		case "ctrl+c":
+			return m, tea.Quit
+
+		case "backspace":
+			m.invalidState = ""
+			if m.curCol > 0 {
+				m.curCol--
+				m.guesses[m.curRow][m.curCol] = ""
+			}
+
+		case "enter":
+			splitGuess := m.guesses[m.curRow]
+			var guess string
+			for _, char := range splitGuess {
+				guess += char
+			}
+			guess = CleanString(guess)
+			if len(guess) != 5 {
+				m.invalidState = "length"
+			} else if !m.dictionary[guess] {
+				m.invalidState = "non-word"
+			} else {
+				// need to check if the word is correct too.
+				m.letterStates[m.curRow] = GetStatesOfLetters(m.answer, guess)
+				// process the colors and all
+				m.curRow++
+				m.curCol = 0
+			}
+
+		default:
+			m.invalidState = ""
+			if m.curCol < 5 {
+				keyStr := msg.String()
+				if len(keyStr) == 1 && unicode.IsLetter(rune(keyStr[0])) {
+					letter := keyStr
+					m.guesses[m.curRow][m.curCol] = CleanString(letter)
+					m.curCol++
+				}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
